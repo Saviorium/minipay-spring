f7f378e5cb17acbb65fe224da4295bb2948048d1
Savior Dronov <saviorium@gmail.com>
2019-02-10 16:27:15 +0300

    Change AccountDAO for better concurrency Using AtomicReference to not synchronize entire insert method

--- core/src/main/java/ru/minipay/dao/AccountDaoInMemoryImpl.java
+++ core/src/main/java/ru/minipay/dao/AccountDaoInMemoryImpl.java
@@ -3,30 +3,34 @@ package ru.minipay.dao;
 import ru.minipay.model.Account;
 
 import java.util.ConcurrentModificationException;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AccountDaoInMemoryImpl implements AccountDao {
-    private final Map<UUID, Account> accounts = new ConcurrentHashMap<>();
+    private final ConcurrentMap<UUID, AtomicReference<Account>> accounts = new ConcurrentHashMap<>();
 
     @Override
-    public synchronized void insert(Account account) {
-        UUID id = account.getId();
-        if(accounts.containsKey(id)) {
-            Account oldAcc = accounts.get(id);
-            if(oldAcc.isChangedAfter(account)) {
+    public void insert(Account newAcc) {
+        UUID id = newAcc.getId();
+        AtomicReference<Account> accountRef = accounts.putIfAbsent(id, new AtomicReference<>(newAcc));
+        if(accountRef != null) {
+            Account oldAcc = accountRef.get();
+            if(oldAcc.isChangedAfter(newAcc)) {
+                throw new ConcurrentModificationException("Error: account update collision");
+            }
+            newAcc.updateLastChanged();
+            if(!accountRef.compareAndSet(oldAcc, newAcc)) {
                 throw new ConcurrentModificationException("Error: account update collision");
             }
         }
-        account.updateLastChanged();
-        accounts.put(account.getId(), account);
     }
 
     @Override
     public Account getById(UUID id) {
         if(accounts.containsKey(id)) {
-            return new Account(accounts.get(id));
+            return new Account(accounts.get(id).get());
         } else {
             return null;
         }
